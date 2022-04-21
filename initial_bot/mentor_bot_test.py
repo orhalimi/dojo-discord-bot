82a3945fe4f954d8a853b1d593396e0ae199824e
Godnessy <godnessy@gmail.com>
2022-04-21 22:57:22 +0200

    Added a function that accepts a room name and a user list, creates a private room with the given name and adds the requested users to that room.

--- initial_bot/mentor_bot_test.py
+++ initial_bot/mentor_bot_test.py
@@ -1,16 +1,22 @@
 # bot.py
+import pdb
 import sys
 import os
 import random
 import discord
 import argparse
 from discord.ext import commands
+from discord.utils import get
+
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-t', '--token', dest = 'token', help = "If you don't have the token please ask Eran to send it to you")
-    parser.add_argument('-g', '--guild_name', dest = 'guild', default = "mentorbot_test")
+    parser.add_argument('-t', '--token', dest='token',
+                        help="If you don't have the token please ask Eran to send it to you")
+    parser.add_argument('-g', '--guild_name', dest='guild',
+                        default="mentorbot_test")
     return parser.parse_args()
+
 
 def main():
     parsed_args = parse_arguments()
@@ -19,15 +25,49 @@ def main():
         sys.exit()
 
     intents = discord.Intents.all()
-    discord.member = True
-    bot = commands.Bot(command_prefix='!', intents = intents)
+    bot = commands.Bot(command_prefix='!', intents=intents)
 
     @bot.event
     async def on_ready():
         print(f'{bot.user.name} has connected to Discord!')
 
-    # Send a gretting to members upon joining the server
-    @bot.event
+    @bot.command(name='private')
+    # @commands.has_role('admin')
+    async def make_private_channel(ctx, room_name, *members: discord.Member,):
+        '''
+        Takes 2 parameters: A room name and a list of tagged members to be added to the room.
+        Creates a private room with the room name and addes the members to that room.
+        Room name must not already exist in the server.
+        '''
+
+        if room_name == None:
+            await ctx.send(f'Room name is empty, please add a room name to the command (after the last member to the be added to the room')
+            return
+        else:
+            guild = ctx.guild
+            overwrites = {
+                guild.default_role: discord.PermissionOverwrite(read_messages=False),
+                guild.me: discord.PermissionOverwrite(read_messages=True),
+            }
+            member_list = []
+            for member in members:
+                member_list.append(member.name)
+                overwrites[member] = discord.PermissionOverwrite(
+                    read_messages=True)
+
+            existing_channels = discord.utils.get(
+                guild.channels, name=room_name)
+            if not existing_channels:
+                members_names = (',').join(member_list)
+                channel = await guild.create_text_channel(room_name, overwrites=overwrites)
+                await ctx.send(f'Creating a private channel called {room_name} and adding {members_names} to it.')
+            else:
+                await ctx.send(f'A room with the name: {room_name} already exists. Please choose a different name.')
+
+
+# Send a gretting to members upon joining the server
+
+    @ bot.event
     async def on_member_join(member):
         await member.create_dm()
         await member.dm_channel.send(
@@ -35,7 +75,7 @@ def main():
         )
 
     # Reply to a !99 message with a random Peralta quote
-    @bot.command(name='99', help='Responds with a random quote from Brooklyn 99')
+    @ bot.command(name='99', help='Responds with a random quote from Brooklyn 99')
     async def nine_nine(ctx):
         brooklyn_99_quotes = [
             'I\'m the human form of the 💯 emoji.',
@@ -51,7 +91,7 @@ def main():
 
     # A !roll_dice message should be accompanied with 2 numbers - the 1st for the number of dice and the 2nd for the number of sides each die has
     # Roll the dice and return the sum of random numvers received
-    @bot.command(name='roll_dice', help='Simulates rolling dice.')
+    @ bot.command(name='roll_dice', help='Simulates rolling dice.')
     async def roll(ctx, number_of_dice: int, number_of_sides: int):
         dice = [
             str(random.choice(range(1, number_of_sides + 1)))
@@ -59,27 +99,13 @@ def main():
         ]
         await ctx.send(', '.join(dice))
 
-    # Create a new channel in the server
-    # The channel's name is received by the caller as an argument
-    # The caller must have an admin role
-    @bot.command(name='create-channel')
-    @commands.has_role('admin')
-    async def create_channel(ctx, channel_name = None):
-        if channel_name == None:
-            await ctx.send("Please add the channel name as an argument")
-        else:
-            guild = ctx.guild
-            existing_channel = discord.utils.get(guild.channels, name=channel_name)
-            if not existing_channel:
-                await ctx.send(f'Creating a new channel: {channel_name}')
-                await guild.create_text_channel(channel_name)
-
-    @bot.event
+    @ bot.event
     async def on_command_error(ctx, error):
         if isinstance(error, commands.errors.CheckFailure):
             await ctx.send('You do not have the correct role for this command.')
 
     bot.run(parsed_args.token)
 
+
 if __name__ == "__main__":
-  main()
+    main()
