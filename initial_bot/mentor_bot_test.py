4db27c1c526b7ad2efe386a14b643f4e45715652
Godnessy <godnessy@gmail.com>
2022-04-30 23:41:17 +0200

    Changed the function to accept a list of members intead of a set amount, beautified the return message

--- initial_bot/mentor_bot_test.py
+++ initial_bot/mentor_bot_test.py
@@ -30,16 +30,16 @@ def main():
     @bot.event
     async def on_ready():
         print(f'{bot.user.name} has connected to Discord!')
-        print('//////////////////')
 
     @bot.command(name='private')
-    @commands.has_role('admin')
-    async def make_private_channel(ctx, member1: discord.Member, member2: discord.Member, member3: discord.Member, room_name: str = None):
+    # @commands.has_role('admin')
+    async def make_private_channel(ctx, room_name, *members: discord.Member,):
         '''
-        Takes 4 parameters: 3 members and a room name.
+        Takes 2 parameters: A room name and a list of tagged members to be added to the room.
         Creates a private room with the room name and addes the members to that room.
         Room name must not already exist in the server.
         '''
+
         if room_name == None:
             await ctx.send(f'Room name is empty, please add a room name to the command (after the last member to the be added to the room')
             return
@@ -47,24 +47,27 @@ def main():
             guild = ctx.guild
             overwrites = {
                 guild.default_role: discord.PermissionOverwrite(read_messages=False),
-                member1: discord.PermissionOverwrite(read_messages=True),
                 guild.me: discord.PermissionOverwrite(read_messages=True),
-                member1: discord.PermissionOverwrite(read_messages=True),
-                member3: discord.PermissionOverwrite(read_messages=True),
             }
-            existing_channel = discord.utils.get(
+            member_list = []
+            for member in members:
+                member_list.append(member.name)
+                overwrites[member] = discord.PermissionOverwrite(
+                    read_messages=True)
+
+            existing_channels = discord.utils.get(
                 guild.channels, name=room_name)
-            if not existing_channel:
-                channel = await guild.create_text_channel(room_name=room_name, overwrites=overwrites)
-                print(f'Created a room called {room_name}')
-                await ctx.send(f'Creating a channel called {room_name} and adding {member1.name}, {member2.name} and {member3.name} to it.')
+            if not existing_channels:
+                members_names = (',').join(member_list)
+                channel = await guild.create_text_channel(room_name, overwrites=overwrites)
+                await ctx.send(f'Creating a private channel called {room_name} and adding {members_names} to it.')
             else:
                 await ctx.send(f'A room with the name: {room_name} already exists. Please choose a different name.')
 
-    make_private_channel()
 
 # Send a gretting to members upon joining the server
-    @bot.event
+
+    @ bot.event
     async def on_member_join(member):
         await member.create_dm()
         await member.dm_channel.send(
@@ -72,7 +75,7 @@ def main():
         )
 
     # Reply to a !99 message with a random Peralta quote
-    @bot.command(name='99', help='Responds with a random quote from Brooklyn 99')
+    @ bot.command(name='99', help='Responds with a random quote from Brooklyn 99')
     async def nine_nine(ctx):
         brooklyn_99_quotes = [
             'I\'m the human form of the 💯 emoji.',
@@ -88,7 +91,7 @@ def main():
 
     # A !roll_dice message should be accompanied with 2 numbers - the 1st for the number of dice and the 2nd for the number of sides each die has
     # Roll the dice and return the sum of random numvers received
-    @bot.command(name='roll_dice', help='Simulates rolling dice.')
+    @ bot.command(name='roll_dice', help='Simulates rolling dice.')
     async def roll(ctx, number_of_dice: int, number_of_sides: int):
         dice = [
             str(random.choice(range(1, number_of_sides + 1)))
@@ -96,7 +99,7 @@ def main():
         ]
         await ctx.send(', '.join(dice))
 
-    @bot.event
+    @ bot.event
     async def on_command_error(ctx, error):
         if isinstance(error, commands.errors.CheckFailure):
             await ctx.send('You do not have the correct role for this command.')
