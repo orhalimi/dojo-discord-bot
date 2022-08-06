16da2f18c2d161ea22042c5d37704f6e2c31f201
Godnessy <godnessy@gmail.com>
2022-08-06 21:12:36 +0200

    added messages url to display the messages view

--- dbot/urls.py
+++ dbot/urls.py
@@ -1,8 +1,11 @@
+import sys
 from django.contrib import admin
 from django.urls import path, include
 from .api.routes import router
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('messages/', views.messages),
 ]
