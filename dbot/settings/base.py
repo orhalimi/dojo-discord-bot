13246fd562f7bc16c8a3d0a6410d2133dfbe9a1a
Godnessy <godnessy@gmail.com>
2022-09-10 21:39:29 +0200

    edited static files dir

--- dbot/settings/base.py
+++ dbot/settings/base.py
@@ -103,7 +103,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join('/home/bot/dojobot/webapp/'),
+    os.path.join(BASE_DIR, '/webapp/static/'),
 ]
 
 # Default primary key field type
