24a1cf754bb88b0ea86f4d72bb6fef7b149ab47b
Godnessy <godnessy@gmail.com>
2022-09-10 20:53:08 +0200

    changed manage.py settings and added logging to check static files issue

--- manage.py
+++ manage.py
@@ -4,10 +4,9 @@ import os
 import sys
 
 
-
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dbot.settings.dev')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dbot.settings.prod')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
