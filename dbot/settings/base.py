71b5fe6047ba4e298ce3ac60c9fe920268c8486d
Godnessy <godnessy@gmail.com>
2022-09-11 12:42:30 +0200

    set static root

--- dbot/settings/base.py
+++ dbot/settings/base.py
@@ -100,6 +100,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_URL = 'static/'
+STATIC_ROOT = '/home/bot/dojobot/webapp/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, '/home/bot/dojobot/webapp/static/'),
