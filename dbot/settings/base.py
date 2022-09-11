2eda51832d3147e7e124bb165f6e14b2633eacc2
Godnessy <godnessy@gmail.com>
2022-09-11 12:41:10 +0200

    removed static url

--- dbot/settings/base.py
+++ dbot/settings/base.py
@@ -100,10 +100,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '/webapp/static/'),
+    os.path.join(BASE_DIR, '/home/bot/dojobot/webapp/static/'),
 ]
 
 # Default primary key field type
