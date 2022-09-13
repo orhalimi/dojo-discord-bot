3f98aff424ab06fb98b3a91c5092a2203b0e4bb4
Godnessy <godnessy@gmail.com>
2022-09-13 16:05:42 +0200

    added static root

--- dbot/settings/settings.py
+++ dbot/settings/settings.py
@@ -103,7 +103,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOL = None
+STATIC_ROOT = os.path.join(BASE_DIR, 'collectedstatic')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static', 'css')
                     ]
