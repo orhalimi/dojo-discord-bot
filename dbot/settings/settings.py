4654363eca888ed68b99d76b3e7cf704278f56b4
Godnessy <godnessy@gmail.com>
2022-09-13 17:41:44 +0200

    changed static files settings to work with whitenoise

--- dbot/settings/settings.py
+++ dbot/settings/settings.py
@@ -103,11 +103,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'collectedstatic')
-
-STATICFILES_DIRS = []
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -124,3 +119,6 @@ REST_FRAMEWORK = {
     #     'rest_framework.renderers.JSONRenderer',
     # )
 }
+STATIC_URL = '/static/'
+STATICFILES_DIRS = []
+STATIC_ROOT = BASE_DIR / "staticfiles"
