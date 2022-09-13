623670acb7e54564bbfbb3c1d528aae9e9eed66b
Godnessy <godnessy@gmail.com>
2022-09-13 17:33:11 +0200

    Added whitenoise

--- dbot/settings/settings.py
+++ dbot/settings/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 
 ROOT_URLCONF = 'dbot.urls'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Application definition
 INSTALLED_APPS = [
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
