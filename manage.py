1bae9ea6d4be1607d5af3f38f5fe181ed5682718
Godnessy <godnessy@gmail.com>
2022-09-13 15:11:54 +0200

    checking settings

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dbot.settings.base')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dbot.prod')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
