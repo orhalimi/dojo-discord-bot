acdf360d61ef48ed79f925f79b2435d994ef088d
Godnessy <godnessy@gmail.com>
2022-09-13 14:50:55 +0200

    changed to settings.py and changed path to it

---
+++ dbot/settings/settings.py
@@ -0,0 +1,35 @@
+from .base import *
+
+'''Development setting file, assigned in main function'''
+
+DEBUG = False
+
+ALLOWED_HOSTS += [IP_ADDRESS]
+
+# Database
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': f'{BASE_DIR}/logs/django/debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
