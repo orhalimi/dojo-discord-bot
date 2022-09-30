ecddbbd0de74cdced736c79eb3dd82d567db91d4
Godnessy <godnessy@gmail.com>
2022-09-30 15:02:05 +0200

    added logging to test cron jobs

--- run_reminder.py
+++ run_reminder.py
@@ -1,5 +1,12 @@
 from reminder_bot import ReminderBot
+from datetime import datetime
+import logging
 
+now = datetime.now()
+current_time = now.strftime("%D,  %H:%M:%S")
+
+f = open("file.log", "a")
+f.write(f"Executed file on: {current_time}, ")
+f.close()
 rb = ReminderBot()
 rb.start_bot()
-
